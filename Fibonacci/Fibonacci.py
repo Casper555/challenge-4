88e4e81b2945bafcc5efd31a0916a81efcbffb2a
Casper555 <507965@vistacollege.nl>
2022-11-28 13:58:39 +0100

    E

---
+++ Fibonacci/Fibonacci.py
@@ -0,0 +1,7 @@
+N = 15
+fibonacciSeries = [0,1]
+if N>2:
+	for i in range(2, N):
+		nextElement = fibonacciSeries[i-1] + fibonacciSeries[i-2]
+		fibonacciSeries.append(nextElement)
+print(fibonacciSeries)
--- rijbewijs/rijbewijs2.py
+++ rijbewijs/rijbewijs2.py
@@ -1,5 +1,5 @@
 from datetime import date
-date.year
-geboorte = input("Voer je geboortedatum in: ")
+date.today
+geboorte = input(int("Voer je geboortedatum in: "))
 if geboorte > date.today() : print("mag rijden")
 else: print("mag niet rijden")
